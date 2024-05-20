f2a904d863e61ac793a9c0e3cc142e41880f47c8
Joe Rogers <joerogers@users.noreply.github.com>
2024-05-20 14:34:20 -0700

    Update Kotlin

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,5 +2,5 @@
 plugins {
     id("com.android.application") version "8.4.1" apply false
     id("com.android.library") version "8.4.1" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.24" apply false
 }
