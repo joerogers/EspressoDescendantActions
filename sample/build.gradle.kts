bc5285caf685f6f6bf748601abf6044689d22def
Joe Rogers <joerogers@users.noreply.github.com>
2025-03-06 11:36:08 -0800

    Update to AS Meerkat

--- sample/build.gradle.kts
+++ sample/build.gradle.kts
@@ -40,7 +40,7 @@ android {
 dependencies {
     implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.appcompat:appcompat:1.7.0")
-    implementation("androidx.recyclerview:recyclerview:1.3.2")
+    implementation("androidx.recyclerview:recyclerview:1.4.0")
     implementation("androidx.cardview:cardview:1.0.0")
     implementation("com.google.android.material:material:1.12.0")
 
